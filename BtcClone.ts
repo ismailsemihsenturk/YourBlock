4e26d8c3ff460020435348c4c777915c36f14ffa
ismailsemih <59211902+ismailsemih@users.noreply.github.com>
2022-06-10 19:08:48 +0300

    keypair create

--- BtcClone.ts
+++ BtcClone.ts
@@ -1,2 +1,9 @@
+//ts dosyasını "js" olarak ver yoksa çalışmaz. tsc --init yapıp tsconfig aç
+import * as crypto from  "crypto";
 
-console.log("sa")
+const keypair = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+    publicKeyEncoding: { type: 'spki', format: 'pem' },
+    privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
+  });
+console.log("sa "+ keypair.publicKey)
