cb292a18396798d62e7203597e326017ab370012
Metehan Temel <metehantemel@yandex.com>
2022-06-11 17:21:03 +0300

    block, mine

--- BtcClone.js
+++ BtcClone.js
@@ -33,4 +33,63 @@ const keyPair = crypto.generateKeyPairSync('rsa', {
 });
 const sign = crypto.createSign('SHA256');
 sign.update("Test");
-console.log(sign.sign(keyPair.privateKey));
+//console.log(sign.sign(keyPair.privateKey));
+class Block {
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash();
+        this.nonce = 0;
+    }
+    calculateHash() {
+        return crypto.createHmac('sha256', this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).digest('hex');
+    }
+    mineBlock(difficulty) {
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Mined: " + this.hash);
+    }
+}
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+        this.difficulty = 3;
+    }
+    createGenesisBlock() {
+        return new Block(0, "09.06.2022", "Genesis block", "0");
+    }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
+    }
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+let testCoin = new Blockchain();
+console.log("Mining block 1");
+testCoin.addBlock(new Block(1, "10.06.2022", { amount: 4 }));
+console.log("Mining block 2");
+testCoin.addBlock(new Block(2, "11.06.2022", { amount: 3 }));
+console.log(JSON.stringify(testCoin, null, 4));
+console.log(testCoin.isChainValid());
+testCoin.chain[1].data = { amount: 10 };
+console.log(testCoin.isChainValid());
